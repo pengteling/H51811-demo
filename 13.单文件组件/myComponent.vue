d9266a1153d424524826f9c1296abebac42a698d
pengteling <pengteling@gmail.com>
2018-12-08 17:31:08 +0800

    1208

---
+++ 13.单文件组件/myComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    hi,{{ who }}
+  </div>
+</template>
+<script>
+module.exports = {
+  data(){
+    return {
+      who : 'vue'
+    }
+  },
+  computed:{
+    who2(){
+      return this.who + '2'
+    }
+  }
+}
+</script>
+
+<style>
+.hello{
+  font-size: 100px;
+  background-color: #ddd;
+}
+</style>
