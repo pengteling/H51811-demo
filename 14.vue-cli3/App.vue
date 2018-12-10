e82ab2eff49186e04fe6fc5f423844a99684c2f5
pengteling <pengteling@gmail.com>
2018-12-10 20:30:04 +0800

    vue-cli3

---
+++ 14.vue-cli3/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    App - {{ msg }}
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      msg:'hi,vue'
+    }
+  }
+}
+</script>
+<style>
+.app{
+  font-size: 50px;
+}
+</style>
